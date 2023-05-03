3869b8ee02187959c83e75a2b882e1082b756650
Mustapha Garba <mustaphamgarba@outlook.com>
2023-05-03 16:16:21 +0100

    wagtail scaffolding

---
+++ garbatech/home/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
+
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+
